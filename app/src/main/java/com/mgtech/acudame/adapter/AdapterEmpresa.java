0441acbd92ad9c06ee2f6ee1201b014be5c195a8
Genesis Aquino <genesiscaquino.2016@gmail.com>
2020-05-31 13:30:39 -0300

    Ajustando layout da tela de cardapio, home, empresa 2.0

--- app/src/main/java/com/mgtech/acudame/adapter/AdapterEmpresa.java
+++ app/src/main/java/com/mgtech/acudame/adapter/AdapterEmpresa.java
@@ -35,7 +35,7 @@ public class AdapterEmpresa extends RecyclerView.Adapter<AdapterEmpresa.MyViewHo
     public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
         Empresa empresa = empresas.get(i);
         holder.nomeEmpresa.setText(empresa.getNome());
-        holder.categoria.setText(empresa.getCategoria() + " -");
+        holder.categoria.setText(empresa.getCategoria());
         holder.dias.setText(empresa.getDias());
         holder.horario.setText(empresa.getHorario() + " Hrs");
         if(empresa.getStatus() != null) {
