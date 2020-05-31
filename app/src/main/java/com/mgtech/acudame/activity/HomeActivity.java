3cef568b074409b1ae391d6e24d0584a6f25d0f3
Genesis Aquino <genesiscaquino.2016@gmail.com>
2020-05-31 02:05:00 -0300

    Ajustando layout da tela de cardapio, home, empresa

--- app/src/main/java/com/mgtech/acudame/activity/HomeActivity.java
+++ app/src/main/java/com/mgtech/acudame/activity/HomeActivity.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
